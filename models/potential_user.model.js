a4afc6c5bb5fabcbe4fcfff267b80ed72ddc02bd
Tejas Kumthekar <tejas.kumthekar26@gmail.com>
2018-01-09 13:03:29 +0530

    Init test suite (#1) (#2)

--- models/potential_user.model.js
+++ models/potential_user.model.js
@@ -2,16 +2,28 @@ var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 
 var PotentialUserSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
+        required: true,
         unique: true 
     },
-    school: String
+    school: {
+        type: String,
+        required: true
+    }
 }, {
     timestamps: true
 });
+
+autoIncrement.initialize(mongoose.connection);
 
 PotentialUserSchema.plugin(autoIncrement.plugin, { model: 'potentialUser', field: 'potentialUserPublicId', startAt: 1 });
 
---
+++ test/potential_user.js
@@ -0,0 +1,115 @@
+let mongoose = require("mongoose");
+let PotentialUser = require('../models/potential_user.model.js');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server.js');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Potential Users', () => {
+    before((done) => {
+        PotentialUser.remove({}, (err) => { 
+           done();         
+        });     
+    });
+
+    /*
+    * Test the /POST /user/potential/create route
+    */
+    describe('/POST /user/potential/create', () => {
+        it('it should not POST a potential user without first name', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "lastName": "Doe",
+                    "email": "jdoe@email.com",
+                    "school": "hogwarts"
+                })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property('message').eql("Missing Potential User's First Name");
+                    done();
+                });
+        });
+
+        it('it should not POST a potential user without last name', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "firstName": "John",
+                    "email": "jdoe@email.com",
+                    "school": "hogwarts"
+                })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Missing Potential User's Last Name");
+                    done();
+                });
+        });
+
+        it('it should not POST a potential user without email', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "school": "hogwarts"
+                })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Missing Potential User's Email");
+                    done();
+                });
+        });
+
+         it('it should not POST a potential user without school', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "jdoe@email.com"
+                })
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.have.property("message").eql("Missing Potential User's School");
+                    done();
+                });
+        });
+
+        it('it should POST a potential user', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "jdoe@email.com",
+                    "school": "hogwarts"
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property("message").eql("Potential User Details Saved Successfully");
+                    done();
+                });
+        });
+
+        it('it should not POST a potential user with duplicate email', (done) => {
+            chai.request(server)
+                .post('/user/potential/create')
+                .send({
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "jdoe@email.com",
+                    "school": "hogwarts"
+                })
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    res.body.should.have.property("code").eql(11000);
+                    res.body.should.have.property("errmsg");
+                    done();
+                });
+        });
+    });
+});

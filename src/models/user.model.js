50ca6d93f42c0417e0ea7393ea3f61b45be202f8
Tejas Kumthekar <tkumthekar@CHIC02TL4SFG8WN.sea.corp.expecn.com>
2018-02-04 16:37:26 -0600

    Add username with unique referential and custom error

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
 const bcrypt = require('bcrypt');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var UserSchema = mongoose.Schema({
     firstName: {
@@ -23,6 +24,11 @@ var UserSchema = mongoose.Schema({
     password : {
         type: String,
         required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
     phone: {
         type: String
@@ -54,5 +60,6 @@ UserSchema.methods.validatePassword = function(password) {
 autoIncrement.initialize(mongoose.connection);
 
 UserSchema.plugin(autoIncrement.plugin, { model: 'user', field: 'userPublicId', startAt: 1 });
+UserSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('user', UserSchema);

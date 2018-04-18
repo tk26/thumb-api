4e8ab7a61d9aa9461cf1d70a8c8f8afc75c5452a
Tejas Kumthekar <tkumthekar@egencia.com>
2018-04-17 21:10:28 -0500

    Add create drive endpoint with tests

--- src/models/drive.model.js
+++ src/models/drive.model.js
@@ -1,38 +1,22 @@
-var mongoose = require('mongoose');
-var autoIncrement = require('mongoose-auto-increment');
+let mongoose = require('mongoose');
+let locationTypes = require('./types/location.type.js');
+let Location = mongoose.Schema.Types.Location;
+let drivesDB = require('../db/drives.js');
 
 var DriveSchema = mongoose.Schema({
-    user_id: String,
-    user_publicId: String,
-    user_firstName: String,
-    user_lastName: String,
-    from_location: {
-        type: String,
-        required: true
-    },
-    to_location: {
-        type: String,
-        required: true
-    },
-    travel_date: {
-        type: String,
-        required: true
-    },
-    travel_time: [{
-        type: String, 
-        required: true
-    }],
-    seats_available: {
-        type: Number,
-        required: true
-    },
-    comment: String
+    /* Neo4j Properties */
+    userId: String,
+    startLocation: Location,
+    endLocation: Location,
+    travelDate: Date,
+    travelTime: String,
+    availableSeats: Number
 }, {
     timestamps: true
 });
 
-autoIncrement.initialize(mongoose.connection);
-
-DriveSchema.plugin(autoIncrement.plugin, { model: 'drive', field: 'drivePublicId', startAt: 1 });
+DriveSchema.methods.saveDrive = function(drive){
+  return drivesDB.saveDrive(drive);
+};
 
 module.exports = mongoose.model('drive', DriveSchema);

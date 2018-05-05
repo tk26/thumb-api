f842ecc0d0384754a59c40b00b7768a943ba90d0
jdromine <jromines@gmail.com>
2018-05-05 12:54:23 -0400

    Adding admin functions to create spatial layers

--- src/db/neo4jAdmin.js
+++ src/db/neo4jAdmin.js
@@ -1,57 +1,85 @@
 const neo4j = require('../extensions/neo4j.js');
 
-exports.indexExists = async function(description){
-  let indexes = await neo4j.execute('CALL db.indexes()');
-  for(let i=0; i<indexes.records.length; i++){
-    if (indexes.records[i]._fields[0] === description){
+module.exports = class Neo4jAdmin {
+  static async indexExists(description){
+    let indexes = await neo4j.execute('CALL db.indexes()');
+    for(let i=0; i<indexes.records.length; i++){
+      if (indexes.records[i]._fields[0] === description){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static async constraintExists(description){
+    let constraints = await neo4j.execute('CALL db.constraints()');
+    for(let i=0; i<constraints.records.length; i++){
+      if (constraints.records[i]._fields[0] === description){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static async createConstraint(description) {
+    let constraintExists = await Neo4jAdmin.constraintExists(description);
+    if (constraintExists) {
+      return true;
+    } else {
+      return neo4j.execute('CREATE ' + description);
+    }
+  }
+
+  static async createIndex(description) {
+    let indexExists = await Neo4jAdmin.indexExists(description);
+    if (indexExists) {
+      return true;
+    } else {
+      return neo4j.execute('CREATE ' + description);
+    }
+  }
+
+  static async dropConstraint(description) {
+    let constraintExists = await Neo4jAdmin.constraintExists(description);
+    if (constraintExists) {
+      return neo4j.execute('DROP ' + description);
+    } else {
       return true;
     }
   }
-  return false;
-}
 
-exports.constraintExists = async function(description){
-  let constraints = await neo4j.execute('CALL db.constraints()');
-  for(let i=0; i<constraints.records.length; i++){
-    if (constraints.records[i]._fields[0] === description){
+  static async dropIndex(description) {
+    let indexExists = await Neo4jAdmin.indexExists(description);
+    if (indexExists) {
+      return neo4j.execute('DROP ' + description);
+    } else {
       return true;
     }
   }
-  return false;
-}
 
-exports.createConstraint = async function(description) {
-  let constraintExists = await exports.constraintExists(description);
-  if (constraintExists) {
-    return true;
-  } else {
-    return neo4j.execute('CREATE ' + description);
+  static async createLayer(layerName) {
+    const layerExists = await Neo4jAdmin.layerExists(layerName);
+    if (!layerExists){
+      return await neo4j.execute('CALL spatial.addWKTLayer(\'' + layerName + '\', \'wkt\')');
+    }
+    return;
   }
-}
 
-exports.createIndex = async function(description) {
-  let indexExists = await exports.indexExists(description);
-  if (indexExists) {
-    return true;
-  } else {
-    return neo4j.execute('CREATE ' + description);
+  static async removeLayer(layerName) {
+    const layerExists = await Neo4jAdmin.layerExists(layerName);
+    if (layerExists){
+      return await neo4j.execute('CALL spatial.removeLayer(\'' + layerName + '\')');
+    }
+    return;
   }
-}
 
-exports.dropConstraint = async function(description) {
-  let constraintExists = await exports.constraintExists(description);
-  if (constraintExists) {
-    return neo4j.execute('DROP ' + description);
-  } else {
-    return true;
-  }
-}
-
-exports.dropIndex = async function(description) {
-  let indexExists = await exports.indexExists(description);
-  if (indexExists) {
-    return neo4j.execute('DROP ' + description);
-  } else {
-    return true;
+  static async layerExists(layerName) {
+    const layers = await neo4j.execute('CALL spatial.layers()');
+    for(let i=0; i<layers.records.length; i++){
+      if (layers.records[i]._fields[0] === layerName){
+        return true;
+      }
+    }
+    return false;
   }
 }
--- test/db-tests/neo4jAdmin.tests.js
+++ test/db-tests/neo4jAdmin.tests.js
@@ -44,4 +44,61 @@ describe('Neo4j Admin Tasks', () => {
       constraintExists.should.equal(false);
     });
   });
+
+  describe('layerExists', () => {
+    const testLayer = 'testLayer';
+    before(async() => {
+      await admin.createLayer(testLayer);
+    });
+    after(async() => {
+      await admin.removeLayer(testLayer);
+    });
+
+    it('should return false if layer does not exist', async() =>{
+      const layerExists = await admin.layerExists('fakeLayer');
+      layerExists.should.equal(false);
+    });
+
+    it('should return true if layer exists', async() =>{
+      const layerExists = await admin.layerExists(testLayer);
+      layerExists.should.equal(true);
+    });
+  });
+
+  describe('createLayer', () => {
+    const testLayer = 'createLayer';
+    const duplicateLayer = 'dupeLayer';
+    after(async() => {
+      await admin.removeLayer(testLayer);
+      await admin.removeLayer(duplicateLayer);
+    });
+
+    it('should successfully create layer', async() =>{
+      await admin.createLayer(testLayer);
+      const layerExists = await admin.layerExists(testLayer);
+      layerExists.should.equal(true);
+    });
+
+    it('should not throw exception when creating layer that already exists', async() =>{
+      await admin.createLayer(duplicateLayer);
+      await admin.createLayer(duplicateLayer);
+    });
+  });
+
+  describe('removeLayer', () => {
+    const testLayer = 'removeLayer';
+    before(async() => {
+      await admin.createLayer(testLayer);
+    });
+
+    it('should successfully remove layer', async() =>{
+      await admin.removeLayer(testLayer);
+      const layerExists = await admin.layerExists(testLayer);
+      layerExists.should.equal(false);
+    });
+
+    it('should not throw exception when removing layer that does not exist', async() =>{
+      await admin.removeLayer('fakeLayer');
+    });
+  });
 })
